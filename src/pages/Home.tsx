2bc886493e8f1a10d87136befce9166af54467ca
varchodi <rauschenrobert@gmail.com>
2023-12-18 13:43:34 +0300

    island model displayed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,20 @@ import Loader from "../components/Loader"
 import Island from "../models/Island"
 
 const Home = () => {
+    const ajustIslandForscreenSize = () => {
+        let screenScale = null; const screenPosition = [0, -6.5, -43];
+        let rotation = [0.1, 4.7, 0];
+
+        if (window.innerWidth < 768) {
+            screenScale = [0.9, 0.9, 0.9];
+        } else {
+            screenScale = [1,1,1];
+        }
+
+        return [screenScale,screenPosition,rotation]
+    }
+
+    const [islandScale,islandPosition,islandRotation] = ajustIslandForscreenSize();
   return (
     <section className='w-full h-screen relative'>
       {/* <div className="absolute top-28 left-0 right-0 z-0 flex items-center justify-center">
@@ -14,12 +28,18 @@ const Home = () => {
         camera={{near:0.1,far:1000}}
       >
         <Suspense fallback={<Loader/>}>
-            <directionalLight/>
-            <ambientLight/>
-            <pointLight/>
-            <spotLight/>
-            <hemisphereLight/>
-            <Island/>
+            {/* like sunlight  */}
+            <directionalLight position={[1,1,1]} intensity={2}/>
+            {/*uliminate all object without casting shadows */}
+            <ambientLight intensity={0.5}/>
+            {/* <pointLight/> */}
+            {/* <spotLight /> */}
+            <hemisphereLight skyColor={"#b1e1ff"} groundColor={"#000000"} intensity={1}/>
+            <Island
+                position={islandPosition}
+                scale={islandScale}
+                rotation={islandRotation}
+            />
         </Suspense>
       </Canvas>
     </section>
