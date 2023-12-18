bd7f4187686e8d03543f0f02e1e116b41afd672c
varchodi <rauschenrobert@gmail.com>
2023-12-18 12:33:57 +0300

    rotes initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import './index.css'
-
+import { Route,BrowserRouter as Router,Routes} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
 const App = () => {
   return (
-    <div>
-      <h1 className='bg-red-800'>cool</h1>
-    </div>
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/projects' element={<Projects/>} />
+          <Route path='/contact' element={ <Contact/>} />
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
